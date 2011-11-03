4d165284eb5ae01b6044a4cd763fb826e7767cf7
Lance Bradbury <lance.bradbury@colorado.edu>
2011-11-03 02:33:43 -0600

    Instructions, not finished

---
+++ gc/instructions.js
@@ -0,0 +1,23 @@
+goog.provide("gc.instructions");
+
+goog.require('lime.Label');
+goog.require('lime.RoundedRect');
+goog.require('lime.Scene');
+goog.require('lime.Sprite');
+goog.require('rb.Button');
+
+gc.instructions = function() {
+	lime.Scene.call(this);
+	
+	var btn = new rb.Button('Back').setPosition(360, 870).setSize(300, 90);
+    goog.events.listen(btn, 'click', function() {rb.loadMenu()});
+    this.appendChild(btn);
+    
+    var contents = new lime.RoundedRect().setRadius(30).setFill('#fff').setSize(700, 560).setPosition(360, 420);
+    this.appendChild(contents);
+    
+    var txt1 = new lime.Label().setFontSize(18).setSize(280, 100).setPosition(-160, -170).setAlign('left');
+    txt1.setText('Click somewhere on the screen to move there');
+    contents.appendChild(txt1);
+};
+goog.inherits(gc.instructions, lime.Scene);
