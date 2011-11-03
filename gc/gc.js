cf45b9cc40ea578ef83dfdb04958611a668357f1
Jarrett Brown <lightningbladex2@hotmail.com>
2011-11-03 17:55:56 -0600

    Modified main menu to have the High Scores button.  Also added sounds when clicking buttons.

--- gc/gc.js
+++ gc/gc.js
@@ -14,6 +14,8 @@ goog.require('lime.animation.FadeTo');
 goog.require('lime.animation.ScaleTo');
 goog.require('lime.animation.MoveTo');
 goog.require('gc.Button');
+goog.require('lime.audio.Audio');
+goog.require('lime.Sprite');
 
 gc.WIDTH = 460;
 gc.HEIGHT = 320;
@@ -28,24 +30,41 @@ gc.start = function(){
 }
 
 gc.loadMenu = function(){
-
+	var btnSound = new lime.audio.Audio('assets/Sounds/dundun.mp3');
+	
 	var scene = new lime.Scene(),
 	  layer = new lime.Layer().setPosition(gc.WIDTH / 2, gc.HEIGHT/2);
 
 	if(gc.isBrokenChrome()) layer.setRenderer(lime.Renderer.CANVAS);
 
-
 	var background = new lime.Sprite().setFill('assets/grimreaper.png').setPosition(0, 100);
 	background.qualityRenderer = true;
 	layer.appendChild(background);
 	
-	// var startBtn = gc.Button("Start").setPosition(0,0).setSize(150, 90);
+	// Creat button, set its position, and listen for a click event to play a sound
 	var startBtn = gc.makeMenuBtn("Start").setPosition(0,0);
+	goog.events.listen(startBtn, 'click', function() {
+		btnSound.stop();
+		btnSound.play();
+	});
 	layer.appendChild(startBtn);
-	 
+	
+	// Creat button, set its position, and listen for a click event to play a sound 
 	var instrBtn = gc.makeMenuBtn("Instructions").setPosition(0, 50);
+	goog.events.listen(instrBtn, 'click', function() {
+		btnSound.stop();
+		btnSound.play();
+	});
 	layer.appendChild(instrBtn);
-	 
+
+	// Creat button, set its position, and listen for a click event to play a sound 
+	var highBtn = gc.makeMenuBtn("High Scores").setPosition(0, 100);
+	goog.events.listen(highBtn, 'click', function() {
+		btnSound.stop();
+		btnSound.play();
+	});
+	layer.appendChild(highBtn);
+	
 	scene.appendChild(layer);
 	
 	// Set current scene active
