d049b4b8eafacad62925e03e97520be7c0839753
James Blades <jwkblades@gmail.com>
2011-11-15 10:38:48 -0800

    Updated the enemy class to do more than it was originally. Added the following functions: Enemy.move - Move the enemy towards the CPU Enemy.timeStep - The function which should be called each timestep by the game. This is responsible for updating the Enemy object and moving them. Enemy.angleBetween - Returns the angle (in Radians) between an enemy and another object (in this case the CPU). Is called by the Enemy.timeStep function.

--- gc/enemy.js
+++ gc/enemy.js
@@ -1,17 +1,59 @@
 goog.provide('gc.Enemy');
 
 goog.require('lime.Sprite');
+goog.require("goog.math.Coordinate");
 
 gc.Enemy = function(cpu){
 	lime.Sprite.call(this);
 	
 	this.cpu = cpu;
+	var x = Math.random() * gc.WIDTH;
+	var y = Math.random() * gc.HEIGHT;
+
+	if(x < gc.WIDTH / 2){
+		x -= gc.WIDTH / 2 - 50;
+	}
+	else{
+		x += gc.WIDTH / 2 + 50;
+	}
+	if(y < gc.HEIGHT / 2){
+		y -= gc.HEIGHT / 2 - 50;
+	}
+	else{
+		y += gc.HEIGHT / 2 + 50;
+	}
+	this.x = x;
+	this.y = y;
+
 	this.v = 1;
-	this.angle = angle;
+	this.angle = angle; // angle in degrees
+	
 	
 }
 goog.inherits(gc.Enemy, lime.Sprite);
 
 gc.Enemy.prototype.setAngle = function(angle){
 	this.angle = angle;
+}
+
+gc.Enemy.prototype.angleTowards = function(obj){
+	var position = this.getPosition();
+	var other = obj.getPosition();
+	var dy = position.y - other.y;
+	var distance = goog.math.Coordinate.distance(position, other);
+	return Math.asin(dy / distance);
+}
+
+gc.Enemy.prototype.move = function(){
+	this.x += Math.cos(this.angle * Math.PI / 180) * this.v;
+	this.y += Math.sin(this.angle * Math.PO / 180) * this.v;
+	this.setPosition(this.x, this.y);
+}
+
+gc.Enemy.prototype.timeStep = function(){
+	// enemies move towards the CPU
+	var theta = this.angleTowards(cpu);
+	var deg = theta * 180 / Math.PI;
+	this.setAngle(deg);
+	this.move();
 }
