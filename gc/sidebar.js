bac321318710a3efcc872087abab0f7d9058a43f
Valerie <valeriehchang@gmail.com>
2011-11-26 16:19:01 -0700

    sidebar.js now able to alter healthbar, sound buttons works and both buttons now with glossy look.

--- gc/sidebar.js
+++ gc/sidebar.js
@@ -4,13 +4,8 @@ goog.provide('gc.SideBar');
 //request that lime.SideBar definitions are loaded
 goog.require('lime.Button');
 goog.require('lime.GlossyButton');
-goog.require('lime.Circle');
 goog.require('lime.fill.Fill');
 goog.require('lime.Sprite');
-goog.require('lime.animation.ColorTo');
-goog.require('lime.animation.FadeTo');
-goog.require('lime.animation.KeyframeAnimation');
-goog.require('lime.animation.Spawn');
 
 //new constructor
 gc.SideBar = function (width, height, game, cpu) {
@@ -24,63 +19,69 @@ gc.SideBar = function (width, height, game, cpu) {
 	this.height = height;
 	this.setFill('#000000');
 	this.setSize(width, height);
+	this.healthBarWidth = this.width * 0.5;
+	var healthBarHeight = this.height * 0.65;
+	var btnWidth = this.width * 0.65;
+	var btnHeight = this.height * 0.10;
+	var btnPosY = (healthBarHeight + btnHeight + 26) / 2;
 	
 	//initial healthBar values
 	var gradient = new lime.fill.LinearGradient().setDirection(0, 0, 0, 1).
 		addColorStop(0, 255, 0, 0, 1).addColorStop(0.5, 255, 255, 0, 1).
 		addColorStop(1, 0, 255, 0, 1);
-	var healthBar = new lime.RoundedRect().setSize(width * 0.5, height * 0.65).
-		setPosition(0, 0).setFill(gradient).setAnchorPoint(0.5, 0.5);
-	this.appendChild(healthBar);
-
+	this.healthBar = new lime.RoundedRect().
+		setSize(this.healthBarWidth, healthBarHeight).setFill(gradient);
+	this.appendChild(this.healthBar);
+	
 	//initial mask values
-	this.mask = new lime.Sprite().setSize(width, 0).setAnchorPoint(0.5, 0.5).
-		setPosition(0, height * 0.32);
+	this.mask = new lime.Sprite().setSize(this.healthBarWidth, 0).
+		setPosition(0, (healthBarHeight / 2));
 	this.appendChild(this.mask);
-
-	healthBar.setMask(this.mask);
-
+	this.healthBar.setMask(this.mask);
+	
 	//initial soundButton values
-	var isSoundOnOff = false;
-	//var bgMusic = new lime.audio.Audio('assets/Sounds/Song6.mp3');
-	var soundBtnUp = new lime.Sprite().setFill('assets/soundButton.gif').
-		setSize(25, 25);
-	var soundBtnDown = new lime.Sprite().setFill('#CDB7B5').
-		setSize(25, 25);
-	var soundBtn = new lime.Button(soundBtnUp, soundBtnDown).
-		setPosition(0, -132);
-	// console.log(soundBtn.getState());
+	var bgMusic = new lime.audio.Audio('assets/Sounds/Song6.mp3');
+	var isMusicPlaying = bgMusic.isPlaying();
+	
+	var soundBtnBg = new lime.Sprite().setFill('assets/sound_0.png').
+		setSize(25, 20).setPosition(0, -btnPosY);
+	this.appendChild(soundBtnBg);
+	
+	var soundBtn = new lime.GlossyButton(' ').
+		setPosition(0, -btnPosY).setSize(btnWidth, btnHeight).
+		setColor('#404040').setOpacity(0.85);
 	this.appendChild(soundBtn);
+	
 	goog.events.listen(soundBtn, ['touchstart', 'mousedown'], function () {
-		//if(!this.isSoundOnOff) {
-			console.log(isSoundOnOff);
-			//this.soundButton.setFill('#8B3626');
-			//this.bgMusic.stop();
-			//this.bgMusic.play();
-			// this.isSoundOnOff = true;
-			//gc.Game.replaceScene(this);
-		//} else {
-			// console.log(this.isSoundOnOff);
-			//this.soundButton.setFill('images/soundButton.gif');
-			//this.bgMusic.stop();
-			// this.isSoundOnOff = false;
-			// gc.Game.replaceScene(this);
-		// }
+		if (!isMusicPlaying) {
+			soundBtnBg.setFill('assets/sound_1.png');
+			bgMusic.stop();
+			bgMusic.play();
+			isMusicPlaying = true;
+		} else {
+			soundBtnBg.setFill('assets/sound_0.png');
+			bgMusic.stop();
+			isMusicPlaying = false;
+		}
     });
 
 	//initial cpuOverButton values
-	var cpuOverBtnUp = new lime.Sprite().setSize(40, 40).
-		setFill('#660000');
-	var cpuOverBtnDown = new lime.Sprite().setSize(40, 40).
-		setFill('#5c5c5c');
-	var cpuOverBtn = new lime.Button().
-		setPosition(0, 132).setUpState(cpuOverBtnUp).setDownState(cpuOverBtnDown);
-		cpuOverBtn.appendChild(cpuOverBtnUp);
-		cpuOverBtn.appendChild(cpuOverBtnDown);
-		this.appendChild(cpuOverBtn);
-	//console.log(isSoundOnOff);
+	var cpuOverState = false;
+	
+	var cpuOverBtn = new lime.GlossyButton('OC').setSize(btnWidth, btnHeight).
+		setPosition(0, btnPosY).setColor('#4359C4');
+	this.appendChild(cpuOverBtn);
+		
 	goog.events.listen(cpuOverBtn, ['touchstart', 'mousedown'], function () {
-		console.log(isSoundOnOff);
+		if (!cpuOverState) {
+			cpuOverState = true;
+			cpuOverBtn.setColor('#D71413').setText('!!!');
+			// insert overclock mode turned on code here
+		} else {
+			cpuOverState = false;
+			cpuOverBtn.setColor('#4359C4').setText('OC');
+			// insert overclock mode turned off code here
+		}
 	});
 };
 
@@ -89,26 +90,10 @@ goog.inherits(gc.SideBar, lime.Sprite);
 
 // new method for bar visibility
 gc.SideBar.prototype.updateBar = function () {
-
-	var heightGrowth = (this.cpu.getStatus()*0.01)*this.height;
-
-	this.mask.setSize(this.width, heightGrowth);
+	var heightGrowth = (100 - this.cpu.getStatus()) * 10;
+	this.mask.setSize(this.healthBarWidth, heightGrowth);
 };
 
-//add some interaction
-gc.SideBar.prototype.turnSoundOnOff = function () {
-	if(!isSoundOnOff) {
-		this.setFill('#8B3626');
-		this.bgMusic.stop();
-		this.bgMusic.play();
-		this.isSoundOnOff = true;
-	} else {
-		this.setFill('images/soundButton.gif');
-		this.bgMusic.stop();
-		this.isSoundOnOff = false;
-	}
-};
-
-/*gc.Game.prototype.overclockOnOff = function() {
+/*gc.SideBar.prototype.overclockOnOff = function() {
 
 }*/
