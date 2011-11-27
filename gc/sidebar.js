f173a7cd76f7f6f0c0c28e48f464573e4aca9719
Valerie <valeriehchang@gmail.com>
2011-11-26 17:10:07 -0700

    Fixed issue with updateBar() when screen resized.

--- gc/sidebar.js
+++ gc/sidebar.js
@@ -20,22 +20,22 @@ gc.SideBar = function (width, height, game, cpu) {
 	this.setFill('#000000');
 	this.setSize(width, height);
 	this.healthBarWidth = this.width * 0.5;
-	var healthBarHeight = this.height * 0.65;
+	this.healthBarHeight = this.height * 0.65;
 	var btnWidth = this.width * 0.65;
 	var btnHeight = this.height * 0.10;
-	var btnPosY = (healthBarHeight + btnHeight + 26) / 2;
+	var btnPosY = (this.healthBarHeight + btnHeight + this.height * 0.05) / 2;
 	
 	//initial healthBar values
 	var gradient = new lime.fill.LinearGradient().setDirection(0, 0, 0, 1).
 		addColorStop(0, 255, 0, 0, 1).addColorStop(0.5, 255, 255, 0, 1).
 		addColorStop(1, 0, 255, 0, 1);
 	this.healthBar = new lime.RoundedRect().
-		setSize(this.healthBarWidth, healthBarHeight).setFill(gradient);
+		setSize(this.healthBarWidth, this.healthBarHeight).setFill(gradient);
 	this.appendChild(this.healthBar);
 	
 	//initial mask values
 	this.mask = new lime.Sprite().setSize(this.healthBarWidth, 0).
-		setPosition(0, (healthBarHeight / 2));
+		setPosition(0, (this.healthBarHeight / 2));
 	this.appendChild(this.mask);
 	this.healthBar.setMask(this.mask);
 	
@@ -91,7 +91,9 @@ goog.inherits(gc.SideBar, lime.Sprite);
 // new method for bar visibility
 gc.SideBar.prototype.updateBar = function () {
 	var heightGrowth = (100 - this.cpu.getStatus()) * 10;
-	this.mask.setSize(this.healthBarWidth, heightGrowth);
+	if (heightGrowth <= this.healthBarHeight*2.05) {
+		this.mask.setSize(this.healthBarWidth, heightGrowth);
+	}
 };
 
 /*gc.SideBar.prototype.overclockOnOff = function() {
